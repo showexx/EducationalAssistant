06fc2147d5e4785fe8415903aaadae3556152b52
showex <official.showex@gmail.com>
2023-10-07 19:34:54 +0300

    start

---
+++ src/main/java/com/example/educationalassistant/EducationalAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.example.educationalassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/educationalassistant/EducationalAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.educationalassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
